7a90889a284c3d6b90c2cf095673c2b594f0e5b0
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-12-05 22:53:49 +0500

    node_server auth

--- node_server/db.js
+++ node_server/db.js
@@ -1,8 +1,7 @@
 import { Pool } from "pg";
 import "dotenv/config";
 
-const pool = new Pool({
+export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-module.exports = pool;
--- node_server/routes/auth.js
+++ node_server/routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import "dotenv/config"
-import pool from "../db.js";
+import { pool } from "../db.js";
 
 
 const router = express.Router();
@@ -84,4 +84,4 @@ router.post('/signin', async (req, res) => {
   }
 });
 
-module.exports = router;
+export default router;
